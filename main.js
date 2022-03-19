0d23bdefe7351f6e432eb5b343ad28b8bdd24eb7
SuhanaCode <87379808+SuhanaCode@users.noreply.github.com>
2022-03-19 15:28:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function preload(){
+}
+
+function setup() {
+    canvas = createCanvas(500,400);
+    canvas.position(70,200);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video,100,80,300,250);
+
+    stroke(0, 0, 100);
+    fill(0, 0, 100);
+    circle(40,40,50);
+    circle(460,40,50);
+    circle(40,360,50);
+    circle(460,360,50);
+
+
+    stroke(0, 100, 0);
+    fill(0, 100, 0);
+
+    rect(63, 27, 375, 25);
+    rect(63, 347, 375, 25);
+    rect(27, 63, 25, 275);
+    rect(448, 63, 25, 275);
+}
+
+function take_snapshot(){
+    save("heehee.png");
+}
